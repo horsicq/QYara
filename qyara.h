d95f3e73ba53b519c2ff4530847b59b7d9f7c6fe
horsicq <horsicq@gmail.com>
2019-06-27 17:59:12 +0200

    Fix

--- qyara.h
+++ qyara.h
@@ -1,3 +1,23 @@
+// Copyright (c) 2019 hors<horsicq@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
 #ifndef QDISASM_H
 #define QDISASM_H
 
@@ -28,13 +48,10 @@ public:
 
     static RESULT scanFile(QString sFileName,QString sRules);
     static QString toText(RESULT *pResult);
-private:
 
 signals:
     void errorMessage(QString sText);
-public slots:
 
-protected:
 private:
     bool bRulesLoaded;
     YR_COMPILER *yrCompiler;
